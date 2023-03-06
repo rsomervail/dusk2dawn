aab6ec2effb4d5ce72f6f247d7672c345c8cea61
rick <r.somervail@gmail.com>
2023-03-06 15:58:36 +0100

    various quality of life upgrades 2
    made so that asr_useGPU is not considered when comparing whether datasets are compatible to merge at group-level

--- d2d_checkSameRun.m
+++ d2d_checkSameRun.m
@@ -7,7 +7,8 @@
 function flag = d2d_checkSameRun(EEG)
     for f = 1:length(EEG)
         temp = EEG(f).etc.dusk2dawn.cfg;
-        d2d{f} = rmfield( temp, {'origFile','cleanFiles'} );   
+        d2d{f} = rmfield( temp, {'origFile','cleanFiles', 'asr_useGPU'} );   
+        d2d{f} = rmfield( d2d{f},  );
     end
     flag = isequaln(d2d{:});
     

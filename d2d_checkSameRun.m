ba33bbdf51c14f592cb9b73e36fe49ed05ed0402
rick <r.somervail@gmail.com>
2023-04-11 13:45:41 +0200

    improved checking of same D2D settings across datasets

--- d2d_checkSameRun.m
+++ d2d_checkSameRun.m
@@ -5,15 +5,38 @@
 % Author: Richard Somervail, Istituto Italiano di Tecnologia, 2022
 %           www.iannettilab.net 
 function flag = d2d_checkSameRun(EEG)
-    for f = 1:length(EEG)
+
+% concat all d2d settings
+    nfiles = length(EEG);
+    d2d  = cell(nfiles,1);
+    pars = d2d;
+    splitByStage = nan(nfiles,1);
+    for f = 1:nfiles
         temp = EEG(f).etc.dusk2dawn.cfg;
-        d2d{f} = rmfield( temp, {'origFile','cleanFiles', 'asr_useGPU'} );   
+        d2d{f} = rmfield( temp, {'origFile','cleanFiles', 'asr_useGPU', 'asr_MaxMem', 'savePath'} );  
+        splitByStage(f) = d2d{f}.splitByStage;
+        pars{f} = temp.pars;
     end
+   
+    % if split by stage inactive the ignore the stagecodes field
+    if sum(splitByStage) ~= nfiles
+        for f = 1:nfiles
+            d2d{f} = rmfield( d2d{f}, 'stageCodes' );
+        end
+    end
+   
+    % check for matching d2d settings
     flag = isequaln(d2d{:});
-    
     if ~flag
-        errordlg2('Datasets do not have matching ASR parameters, run again with the same settings ' ...
-            ,'Error: datasets cannot be merged');
-        error 'Error: datasets are not compatible'
+        errordlg2('Datasets do not seem to have matching ASR parameters, run again with the same settings ' ...
+            ,'Warning: datasets may not be compatible, this may cause errors when plottin');
     end
+   
+    % check for matching d2d varied parameters (i.e. the most crucial check)
+    if ~isequaln(pars{:})
+        errordlg2('Datasets do not seem to have matching *varied* ASR parameters, run again with the same settings ' ...
+            ,'Error: plotting validation metrics will not be possible if datasets do not share the same varied parameters');
+        error 'Error: plotting validation metrics will not be possible if datasets do not share the same varied parameters';
+    end
+
 end
